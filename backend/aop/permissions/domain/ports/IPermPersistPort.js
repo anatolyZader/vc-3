1ac37ebb88da0e6e8680ce0f9140c03a43b9dd54
anatolyZader <myzader@gmail.com>
2024-12-05 14:39:46 +0000

    clever reouters registration

--- backend/aop/permissions/domain/ports/IPermPersistPort.js
+++ backend/aop/permissions/domain/ports/IPermPersistPort.js
@@ -17,6 +17,14 @@ class IPermPersistPort {
     throw new Error('Method not implemented: saveRole(role)');
   }
 
+  async deleteRole(roleId) {
+    throw new Error('Method not implemented: deleteRole(roleId)');
+  }
+
+  async findAllRoles() {
+    throw new Error('Method not implemented: findAllRoles()');
+  }
+
   // Resource Methods
   async findResourceById(resourceId) {
     throw new Error('Method not implemented: findResourceById(resourceId)');
@@ -24,6 +32,10 @@ class IPermPersistPort {
 
   async saveResource(resource) {
     throw new Error('Method not implemented: saveResource(resource)');
+  }
+
+  async deleteResource(resourceId) {
+    throw new Error('Method not implemented: deleteResource(resourceId)');
   }
 
   // Permission Methods
@@ -35,6 +47,10 @@ class IPermPersistPort {
     throw new Error('Method not implemented: savePermission(permission)');
   }
 
+  async deletePermission(roleId, resourceId) {
+    throw new Error('Method not implemented: deletePermission(roleId, resourceId)');
+  }
+
   // Policy Methods
   async findPolicyById(policyId) {
     throw new Error('Method not implemented: findPolicyById(policyId)');
@@ -44,8 +60,12 @@ class IPermPersistPort {
     throw new Error('Method not implemented: savePolicy(policy)');
   }
 
-  async findPolicyForResource(resourceId) {
-    throw new Error('Method not implemented: findPolicyForResource(resourceId)');
+  async deletePolicy(policyId) {
+    throw new Error('Method not implemented: deletePolicy(policyId)');
+  }
+
+  async findAllPolicies() {
+    throw new Error('Method not implemented: findAllPolicies()');
   }
 }
 
