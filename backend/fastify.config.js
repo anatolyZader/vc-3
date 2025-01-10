bc888929a520ae0582fc681911e3269f64761737
anatolyZader <myzader@gmail.com>
2025-01-10 16:59:40 +0000

    commit before introducing docker

--- backend/fastify.config.js
+++ backend/fastify.config.js
@@ -5,14 +5,15 @@ const fs = require('fs');
 const config = require('./config');
 
 module.exports = {
-  server: {
-    https: {
-      key: fs.readFileSync(config.https.keyPath),
-      cert: fs.readFileSync(config.https.certPath),
-    },
-    port: config.server.port,
-    host: config.server.host,
-  },
+    server: {
+        https: {
+          key: fs.readFileSync(config.https.keyPath),
+          cert: fs.readFileSync(config.https.certPath),
+        },
+        // port: config.server.port || 443,
+        port: 443,
+        host: '0.0.0.0', 
+      },
   options: {
     logger: config.logger,
   },
