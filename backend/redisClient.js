a5b78fa622862c336450f25a93130f787ac70f38
anatolyZader <myzader@gmail.com>
2025-03-13 12:22:03 +0000

    cloud sql proxy added

--- backend/redisClient.js
+++ backend/redisClient.js
@@ -2,7 +2,7 @@
 const Redis = require('ioredis');
 
 const redisClient = new Redis({
-  host: '10.6.8.35', // primary endpoint
+  host: '10.2.149.35', // primary endpoint
   port: 6379,
   connectionTimeout: 1000 
 });
