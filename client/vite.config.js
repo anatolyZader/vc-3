a20e4f1c83523fa2d09d26f772e4d39b1f3eddbe
anatolyZader <myzader@gmail.com>
2025-06-11 10:57:37 +0000

    bug fixed - api prefix added to  dev node_env redirect uris

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,5 +5,14 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',  
+  base: '/',
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',  
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  }
 });
